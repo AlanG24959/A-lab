5714cf51ffc8c4fd64f9b36d93ed531535fbc033
AlanG24959 <ga83679@mail.harpercollege.edu>
2025-11-02 22:22:32 -0600

    Finsihed

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -5,6 +5,27 @@
 
 #include "../src/hello.hpp"
 
-TEST_CASE( "it returns Hello World" ) {
-    REQUIRE( hello() == "Hello World!" );
+TEST_CASE("Basic Gradebook Tests") {
+    Gradebook gb;
+
+    gb.addStudent("Alan", "Guerrero", "1001");
+    gb.addStudent("Malenia", "Maliketh", "1002");
+
+    REQUIRE(gb.giveGrade("1001", "HW1", 95) == true);
+    REQUIRE(gb.giveGrade("1002", "HW1", 88.5) == true);
+    REQUIRE(gb.giveGrade("9999", "HW1", 90) == false);
+    REQUIRE(gb.giveGrade("1001", "HW2", -5) == false);
+
+    auto report = gb.printReport();
+    REQUIRE(report.find("Alan Guerrero") != std::string::npos);
+    REQUIRE(report.find("Malenia Maliketh") != std::string::npos);
+    REQUIRE(report.find("HW1: 95") != std::string::npos);
+    REQUIRE(report.find("HW1: 88.5") != std::string::npos);
 }
+
+
+
+
+
+
+
